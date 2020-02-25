115e0a5fc639f35348e64794cfb484d353205cea
Ilya Kovalyov <ilyakov@byui.edu>
2020-02-25 13:27:26 -0700

    finished w08

--- week08/assign08.js
+++ week08/assign08.js
@@ -1,82 +1,48 @@
-
-
 //ON INPUT!!!!
+function checkForm(e) {
+  let myForm = document.getElementById('myForm');
+  myForm.classList.add('checked');
 
+  let valid = myForm.checkValidity();
 
-//calculates the monthly loan based on input
-function calculateLoan(isClicked = true) {
-  //initialize the form and the output
-  let form = document.getElementById("myForm");
-  let output = document.getElementById("payment");
-
-  //get fields references
-  let aprRef = document.getElementById("apr");
-  let termRef = document.getElementById("term");
-  let amountRef = document.getElementById("amount");
-
-  //retrieve values from fields
-  apr = aprRef.value;
-  term = termRef.value;
-  amount = amountRef.value;
-
-  //wait untill all fields are filled
-  if (!isClicked && (apr === "" || amount === "" || term === "")) {
-    return;
+  if (valid) {
+    setTimeout(() => document.getElementById('first_name').focus(), 1000);
   }
 
-  if (!form.checkValidity()) {
+  return valid;
+}
 
-    //set custom validity
-    if (!Number(apr) || Number(apr) <= 0 || Number(apr) > 25) {
-      aprRef.setCustomValidity("Make sure APR is between 0.01 and 25%");
-    } else {
-      aprRef.setCustomValidity("");
-    }
-
-    if (!Number(term) || Number(term) <= 0) {
-      termRef.setCustomValidity("Enter a whole year number");
-    } else {
-      termRef.setCustomValidity("");
-    }
-
-    if (!Number(amount) || Number(amount) <= 0) {
-      amountRef.setCustomValidity("How much is your total?");
-    } else {
-      amountRef.setCustomValidity("");
-    }
+function calculateTotal(e) {
+  if (e.checked) {
+    updateTotal(Number(e.value));
+  } else {
+    updateTotal(-Number(e.value));
   }
+}
 
-  //upon form validity - perform calculations
-  if (form.reportValidity()) {
+function updateTotal(amount) {
+  let totalField = document.getElementById('total');
 
-    //first of all, get correct numbers
-    apr = Number(apr) / 100 / 12;
-    term = Number(term) * 12;
-    amount = Number(amount);
-
-    //calculate the result
-    let result = amount * (apr * Math.pow(1 + apr, term)) / (Math.pow(1 + apr, term) - 1);
-
-    //display the result
-    output.value = (result).toFixed(2);
-  }
-
+  totalField.value = (Number(totalField.value) + Number(amount)).toFixed(2);
 }
 
 //asks for confirmation before resetting the form
 function confirmReset() {
-  let isConfirmed = confirm("Are you sure you want to erase the form?");
+  let isConfirmed = confirm('Are you sure you want to erase the form?');
+
   if (isConfirmed) {
-    document.getElementById("first_name").focus();
+    document.getElementById('first_name').focus();
+    form.classList.remove('checked');
   }
+
   return isConfirmed;
 }
 
 //JQUERY used for navigation (NOT assignment)
 $('#scroll-up').hide();
 
-$(function () {
-  $("#header").load("../lib/header.html");
+$(function() {
+  $('#header').load('../lib/header.html');
 });
 
 $(window).scroll(e => {
@@ -88,7 +54,6 @@ $(window).scroll(e => {
     $('#scroll-up').hide();
   }
 });
-
 
 //form.reportValidity()
 //.setCustomValidity("asdf")
